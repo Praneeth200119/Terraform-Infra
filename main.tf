e3f43f75faaaecd82ca178753f04ed9d0f5e9e58
Praneeth200119 <140323127+Praneeth200119@users.noreply.github.com>
2024-12-02 11:02:17 +0000

    ec2 creation

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "aws_instance" "test" {
+  ami = ami-0614680123427b75e
+  instance_type = "t2.micro"
+}
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "default_region" {
+    description = "AWS region"
+    type        = "string"
+    default     = "ap-south-1" 
+}
+
+variable "vpc_id" {
+  description = "vpc ID"
+  type = "string"
+  default = "vpc-04bb9a2caa4ead833"
+}
+
+variable "security_group_cidr_blocks" {
+  description = "Security group CIDR blocks"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
